c797c2d8294557e100d4965988829dc37aa80906
maulana <maulana@mymindstores.com>
2020-08-13 11:30:15 +0700

    [ADD] Fragment Confirmation

--- app/src/main/java/com/example/exercise_navigation/FragmentInputAmount.kt
+++ app/src/main/java/com/example/exercise_navigation/FragmentInputAmount.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_input_amount.*
 
 /**
@@ -12,7 +14,9 @@ import kotlinx.android.synthetic.main.fragment_input_amount.*
  * Use the [FragmentInputAmount.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentInputAmount : Fragment() {
+class FragmentInputAmount : Fragment(),View.OnClickListener {
+
+    lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +30,20 @@ class FragmentInputAmount : Fragment() {
         return inflater.inflate(R.layout.fragment_input_amount, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        btnSend.setOnClickListener(this)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         tvName.text = "To : ${arguments?.getString("name")}"
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            btnSend -> navController.navigate(R.id.action_fragmentInputAmount_to_fragmentConfirmation)
+        }
     }
 }
